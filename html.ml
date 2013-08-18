c270beca36927328150c420d4e63357b77961303
p4bl0 <r@uzy.me>
2013-08-18 19:48:41 +0200

    css

--- html.ml
+++ html.ml
@@ -1,9 +1,21 @@
 open Batteries ;;
 
 let print_header html fia =
-  Printf.fprintf html "<!DOCTYPE html><html>\n";
-  Printf.fprintf html "<head><title>finja report for %s</title></head>\n" fia;
-  Printf.fprintf html "<body>\n"
+  Printf.fprintf html "<!DOCTYPE html>\
+<html>\
+  <head>\
+    <title>finja report for %s</title>\
+  <style type=\"text/css\">\
+  * { margin:0; padding:0; font-size:1em; }\
+  body { font-size:1em; color:#222; padding:0.5em; }\
+  h1 { font-size:1.2em; padding:0.5em; }\
+  pre { margin:0.5em; padding:0.5em; background-color:#ddd; line-height:150%%; }\
+  strong, em { color:red; }\
+  </style>\
+  </head>\
+  <body>\
+    <h1>finja report for &quot;%s&quot;</h1>\
+" fia fia
 ;;
 
 let print_term html term =
