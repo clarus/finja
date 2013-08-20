86f0737c2fbe583bc2eeccbaea447e9012d7b97e
p4bl0 <r@uzy.me>
2013-08-20 23:58:20 +0200

    analysis

--- analysis.ml
+++ analysis.ml
@@ -2,5 +2,6 @@ open Batteries ;;
 open Computation ;;
 
 let check cond term faulted_term =
-  term = faulted_term
+  let test = Let ("_", term, Let ("@", faulted_term, If (cond, One, Zero))) in
+  Reduction.reduce test = One
 ;;
